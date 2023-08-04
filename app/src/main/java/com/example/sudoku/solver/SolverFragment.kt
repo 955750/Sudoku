899ca49c02b67f35022af1e634072296469e8d17
955750 <019jfuentes@gmail.com>
2023-08-04 13:09:50 +0200

    Seleccionar casilla con color

--- app/src/main/java/com/example/sudoku/solver/SolverFragment.kt
+++ app/src/main/java/com/example/sudoku/solver/SolverFragment.kt
@@ -9,6 +9,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.sudoku.R
 import com.example.sudoku.databinding.FragmentSolverBinding
 
@@ -16,6 +17,7 @@ import com.example.sudoku.databinding.FragmentSolverBinding
 class SolverFragment : Fragment() {
 
     private lateinit var binding: FragmentSolverBinding
+    private val solverViewModel: SolverViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,64 +35,131 @@ class SolverFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val sudokuGrid = binding.sudokuGrid
-        for (row in 1..9) {
+        createSudokuGrid()
+    }
+
+    private fun createSudokuGrid() {
+        for (row in 0..8) {
             val sudokuRow = layoutInflater.inflate(
                 R.layout.sudoku_row_layout,
                 binding.sudokuGrid,
                 false
             ) as LinearLayout
-            sudokuGrid.addView(sudokuRow)
-            for (column in 1..9) {
+            binding.sudokuGrid.addView(sudokuRow)
+            for (column in 0..8) {
                 val numberSquare = layoutInflater.inflate(
                     R.layout.number_square_layout,
                     binding.sudokuGrid,
                     false
                 ) as TextView
-                numberSquare.text = column.toString()
+                numberSquare.text = (column + 1).toString()
                 numberSquare.setOnClickListener {
-                    Log.d(TAG, "row = $row, column = $column")
+                    onSquareClicked(it as TextView, row, column)
                 }
-                setNumberSquareBorders(numberSquare, row, column)
+                setNotSelectedNumberSquareColor(numberSquare, row, column)
                 sudokuRow.addView(numberSquare)
             }
         }
     }
 
-    private fun setNumberSquareBorders(numberSquare: TextView, row: Int, column: Int) {
+    private fun onSquareClicked(
+        selectedSquareInstance: TextView,
+        selectedRow: Int,
+        selectedColumn: Int
+    ) {
+        if (solverViewModel.areCoordinatesInitialised()) {
+            val lastSelectedSquareCoordinates = solverViewModel
+                .lastSelectedSquareCoordinates.value!!
+            val lastSelectedRow = binding.sudokuGrid.getChildAt(
+                lastSelectedSquareCoordinates.first
+            ) as LinearLayout
+            val lastSelectedSquare = lastSelectedRow.getChildAt(
+                lastSelectedSquareCoordinates.second
+            ) as TextView
+            setNotSelectedNumberSquareColor(
+                numberSquare = lastSelectedSquare,
+                row = lastSelectedSquareCoordinates.first,
+                column = lastSelectedSquareCoordinates.second
+            )
+        }
+        setSelectedNumberSquareColor(selectedSquareInstance, selectedRow, selectedColumn)
+        solverViewModel.updateLastSelectedSquareCoordinates(selectedRow, selectedColumn)
+        Log.d(TAG, "row = $selectedRow, column = $selectedColumn")
+    }
+
+    private fun setNotSelectedNumberSquareColor(numberSquare: TextView, row: Int, column: Int) {
         when (column) {
-            1, 4, 7 -> {
+            0, 3, 6 -> {
                 when (row) {
-                    1, 4, 7 -> numberSquare.background = ContextCompat
+                    0, 3, 6 -> numberSquare.background = ContextCompat
                         .getDrawable(requireContext(), R.drawable.num_sq_top_left_bg)
-                    2, 3, 5, 6, 8 -> numberSquare.background = ContextCompat
+                    1, 2, 4, 5, 7 -> numberSquare.background = ContextCompat
                         .getDrawable(requireContext(),
                             R.drawable.num_sq_middle_col__right_col__top_bg)
-                    9 -> numberSquare.background = ContextCompat
+                    8 -> numberSquare.background = ContextCompat
                         .getDrawable(requireContext(), R.drawable.num_sq_bottom_left_bg)
                 }
             }
-            2, 3, 5, 6, 8 -> {
+            1, 2, 4, 5, 7 -> {
                 when (row) {
-                    1, 4, 7 -> numberSquare.background = ContextCompat
+                    0, 3, 6 -> numberSquare.background = ContextCompat
                         .getDrawable(requireContext(), R.drawable.num_sq_top__middle_right_bg)
-                    2, 3, 5, 6, 8 -> numberSquare.background = ContextCompat
+                    1, 2, 4, 5, 7 -> numberSquare.background = ContextCompat
                         .getDrawable(requireContext(),
                             R.drawable.num_sq_middle_col__right_col_bg)
-                    9 -> numberSquare.background = ContextCompat
+                    8 -> numberSquare.background = ContextCompat
                         .getDrawable(requireContext(),
                             R.drawable.num_sq_middle_col__right_col__bottom_bg)
                 }
             }
-            9 -> {
+            8 -> {
                 when (row) {
-                    1, 4, 7 ->  numberSquare.background = ContextCompat
+                    0, 3, 6 ->  numberSquare.background = ContextCompat
                         .getDrawable(requireContext(), R.drawable.num_sq_top_right_bg)
-                    2, 3, 5, 6, 8 -> numberSquare.background = ContextCompat
+                    1, 2, 4, 5, 7 -> numberSquare.background = ContextCompat
                         .getDrawable(requireContext(),
                             R.drawable.num_sq_middle_col__right_col_right_bg)
-                    9 -> numberSquare.background = ContextCompat
+                    8 -> numberSquare.background = ContextCompat
                         .getDrawable(requireContext(), R.drawable.num_sq_bottom_right_bg)
+                }
+            }
+        }
+    }
+
+    private fun setSelectedNumberSquareColor(numberSquare: TextView, row: Int, column: Int) {
+        when (column) {
+            0, 3, 6 -> {
+                when (row) {
+                    0, 3, 6 -> numberSquare.background = ContextCompat
+                        .getDrawable(requireContext(), R.drawable.num_sq_top_left_bg_selected)
+                    1, 2, 4, 5, 7 -> numberSquare.background = ContextCompat
+                        .getDrawable(requireContext(),
+                            R.drawable.num_sq_middle_col__right_col__top_bg_selected)
+                    8 -> numberSquare.background = ContextCompat
+                        .getDrawable(requireContext(), R.drawable.num_sq_bottom_left_bg_selected)
+                }
+            }
+            1, 2, 4, 5, 7 -> {
+                when (row) {
+                    0, 3, 6 -> numberSquare.background = ContextCompat
+                        .getDrawable(requireContext(), R.drawable.num_sq_top__middle_right_bg_selected)
+                    1, 2, 4, 5, 7 -> numberSquare.background = ContextCompat
+                        .getDrawable(requireContext(),
+                            R.drawable.num_sq_middle_col__right_col_bg_selected)
+                    8 -> numberSquare.background = ContextCompat
+                        .getDrawable(requireContext(),
+                            R.drawable.num_sq_middle_col__right_col__bottom_bg_selected)
+                }
+            }
+            8 -> {
+                when (row) {
+                    0, 3, 6 ->  numberSquare.background = ContextCompat
+                        .getDrawable(requireContext(), R.drawable.num_sq_top_right_bg_selected)
+                    1, 2, 4, 5, 7 -> numberSquare.background = ContextCompat
+                        .getDrawable(requireContext(),
+                            R.drawable.num_sq_middle_col__right_col_right_bg_selected)
+                    8 -> numberSquare.background = ContextCompat
+                        .getDrawable(requireContext(), R.drawable.num_sq_bottom_right_bg_selected)
                 }
             }
         }
---
+++ app/src/main/java/com/example/sudoku/solver/SolverViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.sudoku.solver
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SolverViewModel : ViewModel() {
+    private val _lastSelectedLastSquareCoordinates: MutableLiveData<Pair<Int, Int>> =
+        MutableLiveData(Pair(-1, -1))
+    val lastSelectedSquareCoordinates: LiveData<Pair<Int, Int>> =
+        _lastSelectedLastSquareCoordinates
+
+    fun updateLastSelectedSquareCoordinates(row: Int, column: Int) {
+        _lastSelectedLastSquareCoordinates.value = Pair(row, column)
+        Log.d(
+            TAG,
+            "row = ${_lastSelectedLastSquareCoordinates.value?.first}, " +
+                 "column = ${_lastSelectedLastSquareCoordinates.value?.second}"
+        )
+    }
+
+    fun areCoordinatesInitialised(): Boolean {
+        if (_lastSelectedLastSquareCoordinates.value != null)
+            return _lastSelectedLastSquareCoordinates.value?.first != -1 &&
+                _lastSelectedLastSquareCoordinates.value?.second != -1
+        return false
+    }
+
+    companion object {
+        const val TAG = "SolverViewModel"
+    }
+}
